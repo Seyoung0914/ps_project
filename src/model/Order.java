8c90db80b0284a4928b08335514146e766e720db
seyoung0914 <seyoung8529@gmail.com>
2026-05-01 04:38:17 +0900

    feat: make Order model

--- src/model/Order.java
+++ src/model/Order.java
@@ -7,7 +7,33 @@ public class Order {
     private int type;
     private Menu menu;
 
+    public Order() {
+    }
+    public Order(int orderId, int status, int quantity, int type, Menu menu) {
+        this.orderId = orderId;
+        this.status = status;
+        this.quantity = quantity;
+        this.type = type;
+        this.menu = menu;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
     public int getStatus(){
         return status;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public Menu getMenu() {
+        return menu;
     }
 }
