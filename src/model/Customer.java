b2593b0d02ffead436c0632ca9240da9c118b7b4
seyoung0914 <seyoung8529@gmail.com>
2026-05-02 14:42:35 +0900

    chore: add comment to explain func, data class

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,62 +1,79 @@
 package model;
 
+// ArrayList를 사용하기 위해 import
+// ArrayList는 여러 개의 데이터를 순서대로 저장할 수 있는 리스트 자료구조
+
 import java.util.ArrayList;
 
 public class Customer {
+
+    // 고객 이름, 아이디, 비밀번호를 저장하는 변수
     private String name, userId, password;
+
+    // 고객이 현재 가지고 있는 잔액
     private int balance;
+
+    // 고객이 주문한 메뉴들을 저장하는 리스트
     private ArrayList<Menu> menuList;
 
-    public Customer(){}
-    public Customer(String name, String userId, String password, int balance){
-        this.name = name;
-        this.userId = userId;
-        this.password = password;
-        this.balance = balance;
+    // 기본 생성자
+    // 객체를 만들 때 값을 바로 넣지 않고 나중에 setter로 넣고 싶을 때 사용 가능
+    public Customer() {
     }
 
+    // 고객 정보를 한 번에 넣어서 객체를 만들 때 사용하는 생성자
+    public Customer(String name, String userId, String password, int balance) {
+        this.name = name;           // 입력받은 name 값을 현재 객체의 name에 저장
+        this.userId = userId;       // 입력받은 userId 값을 현재 객체의 userId에 저장
+        this.password = password;   // 입력받은 password 값을 현재 객체의 password에 저장
+        this.balance = balance;     // 입력받은 balance 값을 현재 객체의 balance에 저장
+
+        // 주문 메뉴 리스트 초기화
+        this.menuList = new ArrayList<>();
+    }
+
+    // getter, setter
     public String getName() {
         return name;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public ArrayList<Menu> getMenuList() {
-        return menuList;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
+    public int getBalance() {
+        return balance;
+    }
+
     public void setBalance(int balance) {
         this.balance = balance;
     }
+
+    public ArrayList<Menu> getMenuList() {
+        return menuList;
+    }
+
     public void setMenuList(ArrayList<Menu> menuList) {
         this.menuList = menuList;
     }
 
-    public void setMenu(Menu menu){
-        if(balance<menu.getPrice()){System.out.println("잔고가 부족합니다."); return;}
-        balance -= menu.getPrice();
+    public void setMenu(Menu menu) {
         menuList.add(menu);
     }
 
--- src/model/Menu.java
+++ src/model/Menu.java
@@ -1,24 +1,47 @@
 package model;
 
 public class Menu {
-    private String name;
-    private int type;
-    private int price;
-    private String description;
-    private int stock;
-    private static final String[] RESTAURANTS = {"든든한동", "따스한동", "Fry Fry", "H:Plate", "Asian Market"};
 
+    // 식당 이름들을 배열로 저장
+    // type 값이 1이면 RESTAURANTS[0]인 "든든한동"을 의미함
+    // type 값이 2이면 RESTAURANTS[1]인 "따스한동"을 의미함
+    private static final String[] RESTAURANTS = {
+            "든든한동", "따스한동", "Fry Fry", "H:Plate", "Asian Market"
+    };
+    // 메뉴 이름을 저장하는 변수
+    // 예: "김치찌개", "돈까스", "라면"
+    private String name;
+    // 식당 타입을 숫자로 저장하는 변수
+    // 1: 든든한동
+    // 2: 따스한동
+    // 3: Fry Fry
+    // 4: H:Plate
+    // 5: Asian Market
+    private int type;
+    // 메뉴 가격을 저장하는 변수
+    // 예: 7000, 8500
+    private int price;
+    // 메뉴 설명을 저장하는 변수
+    // 예: "얼큰한 김치찌개", "바삭한 돈까스"
+    private String description;
+    // 메뉴 재고를 저장하는 변수
+    // 여기서는 실제 의미상 "판매 개수"보다는 "남은 재고 개수"에 가까움
+    private int stock;
+
+    // 기본 생성자
     public Menu() {
     }
 
+    // 메뉴 정보를 한 번에 넣어서 Menu 객체를 만들 때 사용하는 생성자
     public Menu(String name, int price, int type, String description, int stock) {
-        this.name = name;
-        this.price = price;
-        this.type = type;
-        this.description = description;
-        this.stock = stock;
+        this.name = name;                 // 입력받은 메뉴 이름을 현재 객체의 name에 저장
+        this.price = price;               // 입력받은 가격을 현재 객체의 price에 저장
+        this.type = type;                 // 입력받은 식당 타입 번호를 현재 객체의 type에 저장
+        this.description = description;   // 입력받은 메뉴 설명을 현재 객체의 description에 저장
+        this.stock = stock;               // 입력받은 재고 개수를 현재 객체의 stock에 저장
     }
 
+    // getter, setter
     public String getName() {
         return name;
     }
@@ -62,7 +85,7 @@ public class Menu {
     @Override
     public String toString() {
         return "음식 이름: " + name +
-                ", 식당: " + RESTAURANTS[type-1] +
+                ", 식당: " + RESTAURANTS[type - 1] +
                 ", 가격: " + price + "원" +
                 ", 설명: " + description +
                 ", 판매 개수: " + stock + "개";
